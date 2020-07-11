317074f564d5b6bd31d27bd8798af46d5304af1b
satya saithi <satyasaithi610@gmail.com>
2020-07-11 10:00:45 +0530

    Updates

---
+++ precache-manifest.39a6d2cde6d621974c28068df1227003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6c8cddd5e38f1fa1c8d4c0834ae860",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "310f6c8f5b9f61f212b1",
+    "url": "/react-task/static/css/main.3a037457.chunk.css"
+  },
+  {
+    "revision": "94bf43eba012d02c97bb",
+    "url": "/react-task/static/js/2.844e7671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task/static/js/2.844e7671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310f6c8f5b9f61f212b1",
+    "url": "/react-task/static/js/main.c602a028.chunk.js"
+  },
+  {
+    "revision": "7a56f723fc9bbcd51127",
+    "url": "/react-task/static/js/runtime-main.8f0e6c8e.js"
+  }
+]);
